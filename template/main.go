b4a3b07bcb600dba839b9bf953ecf08777d73f2e
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2021-12-03 17:11:54 +0000

    input now downloaded too

--- template/main.go
+++ template/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path"
 )
@@ -38,19 +40,25 @@ func init() {
 
 func main() {
 	rootDirName := fmt.Sprintf("day%s", day)
+
+	inputData, err := fetchInput(day)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	switch codeType {
 	case NODE:
-		if err := createForNode(rootDirName); err != nil {
+		if err := createForNode(rootDirName, inputData); err != nil {
 			log.Fatal(err)
 		}
 	case GOLANG:
-		if err := createForGolang(rootDirName); err != nil {
+		if err := createForGolang(rootDirName, inputData); err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func createForNode(rootDirName string) error {
+func createForNode(rootDirName string, inputData []byte) error {
 	defaultFileData := loadNodeJSFile()
 
 	err := os.Mkdir(rootDirName, os.ModePerm)
@@ -69,10 +77,10 @@ func createForNode(rootDirName string) error {
 		}
 	}
 
-	return nil
+	return writeInputData(path.Join(rootDirName, "input"), inputData)
 }
 
-func createForGolang(rootDirName string) error {
+func createForGolang(rootDirName string, inputData []byte) error {
 	defaultFileData := loadDefaultGoFile()
 
 	directories := []string{
@@ -86,7 +94,7 @@ func createForGolang(rootDirName string) error {
 		}
 	}
 
-	return nil
+	return writeInputData(path.Join(rootDirName, "input"), inputData)
 }
 
 func createDirectory(dirName string, data []byte) error {
@@ -122,4 +130,33 @@ func loadNodeJSFile() []byte {
 
 const data = readFileSync('./input', { encoding: 'utf-8' }).trim().split('\n');
 	`)
+}
+
+func fetchInput(day string) ([]byte, error) {
+	client := &http.Client{}
+
+	url := fmt.Sprintf("https://adventofcode.com/2021/day/%s/input", day)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	req.Header.Add("Cookie", fmt.Sprintf("session=%s", os.Getenv("SESSION")))
+	response, err := client.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return data, nil
+}
+
+func writeInputData(filePath string, data []byte) error {
+	return os.WriteFile(filePath, data, os.ModePerm)
 }
