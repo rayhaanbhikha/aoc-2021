4038a37d8d4e03089a3d5f4dc7cb58dc59b5a2a1
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2021-12-07 10:59:15 +0000

    part 2 complete

---
+++ day7-node/part2.js
@@ -0,0 +1,45 @@
+const { readFileSync } = require("fs");
+
+const data = readFileSync('./input', { encoding: 'utf-8' }).trim().split(',').map(Number);
+
+class Crabs {
+  constructor(input) {
+    this.crabs = {};
+    for (const crabPosition of input) {
+      const copies = this.crabs[crabPosition];
+      this.crabs[crabPosition] = copies ? copies + 1 : 1
+    }
+  }
+
+  computeFuelCost(num) {
+    let fuelCost = 0;
+    for (const crab in this.crabs) {
+      const copies = this.crabs[crab];
+      const diff = Math.abs(num - Number(crab));
+      const fuelCostPerCrab = 0.5 * diff * (diff + 1)
+      fuelCost += (fuelCostPerCrab * copies);
+    }
+    return fuelCost;
+  }
+}
+
+const crabPositions = Array.from(new Set(data.sort((a, b) => a - b)));
+const crabs = new Crabs(data);
+
+const minimumFuel = (crabPositions, crabs) => {
+  let leftIndex = 0;
+  let rightIndex = crabPositions.length - 1;
+  while (rightIndex > leftIndex) {
+    const fuelCostLeft = crabs.computeFuelCost(leftIndex);
+    const fuelCostRight = crabs.computeFuelCost(rightIndex);
+    if (fuelCostRight > fuelCostLeft) {
+      rightIndex--
+    } else {
+      leftIndex++
+    }
+  }
+
+  return crabs.computeFuelCost(leftIndex);
+}
+
+console.log(minimumFuel(crabPositions, crabs))
